216493394fb3c4bcd5290f36f906ebc587ab4212
adam woodson <awoodson.contact@gmail.com>
2024-03-05 15:53:53 -0600

    Added the functionality to prevent another code from displaying after entering the first code and then starting to type another. CLR is now required to press after entering your first code in order to display new details. Also changed url for images.

--- client/src/components/VendingMachine.jsx
+++ client/src/components/VendingMachine.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import '../VendorStyle.css';
 import { Link } from 'react-router-dom';
-import DisplayOne from './DisplayOne'; 
+import DisplayOne from './DisplayOne';
 import DisplayAll from './DisplayAll';
 
 function VendingMachine() {
     const [selectedLetter, setSelectedLetter] = useState(null); // State to store the selected letter
     const [selectedNumber, setSelectedNumber] = useState(null); // State to store the selected number
     const [displaySnack, setDisplaySnack] = useState(false); // State to control displaying the snack
-    
+    const [confirmSnack, setConfirmSnack] = useState(false); // Creating a confirm snack to handle the ENT error that keeps occurring. 
+
 
     const buttons = [
         'A', 'B', 'C', 'D', 'E', 'F',
@@ -26,24 +27,26 @@ function VendingMachine() {
     // Function to handle selection of a letter
     const handleLetterSelection = (letter) => {
         audio.play();
-        setSelectedLetter(letter); // Update selected letter
+        if (!confirmSnack) {
+            setSelectedLetter(letter); // Update selected letter
+        }
     };
 
     // Function to handle selection of a number
     const handleNumberSelection = (number) => {
         audio.play();
-        setSelectedNumber(number); // Update selected number
+        if(!confirmSnack) {
+            setSelectedNumber(number); // Update selected letter
+        }
     };
 
     // Function to handle ENT button click
     const handleEntButtonClick = () => {
-        if (selectedLetter && selectedNumber) {
+        if (selectedLetter && selectedNumber && !confirmSnack) {
             audio.play();
-            // Set displaySnack to true to show the snack
+            setConfirmSnack(true); //Set confirm to true. 
             setDisplaySnack(true);
-        } else {
-            audio.play();
-        }
+        };
     };
 
     // Function to handle CLR button click
@@ -52,6 +55,7 @@ function VendingMachine() {
         setSelectedLetter(null); // Clear selected letter
         setSelectedNumber(null); // Clear selected number
         setDisplaySnack(false); // Hide displayed snack
+        setConfirmSnack(false); // set snack confirm to false. 
     };
 
 
@@ -71,13 +75,14 @@ function VendingMachine() {
                 </div>
             </div>
             <div className="display-all-snacks">
-                <DisplayAll/>
-            <div className='snack-details'>
-                {displaySnack && selectedLetter && selectedNumber && <DisplayOne code={`${selectedLetter}${selectedNumber}`} />}
-            </div>
+                <DisplayAll />
+                <div className='snack-details'>
+                    {displaySnack && selectedLetter && selectedNumber && <DisplayOne code={`${selectedLetter}${selectedNumber}`} />}
+                </div>
             </div>
         </div>
     );
 }
+
 
 export default VendingMachine;
